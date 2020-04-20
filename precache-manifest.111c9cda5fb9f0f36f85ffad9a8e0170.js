6b04f2871621cd43a9b437b3a64db2ac3d236dd9
jasprweb <swaysmods@gmail.com>
2020-04-20 13:53:37 +0100

    Updates

---
+++ precache-manifest.111c9cda5fb9f0f36f85ffad9a8e0170.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd3b95299c51f1d94a019754ea199e9d",
+    "url": "/covid-19-live/index.html"
+  },
+  {
+    "revision": "1da62dc2e0e0ba6fd97e",
+    "url": "/covid-19-live/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4f18dec838fbc37a2ce6",
+    "url": "/covid-19-live/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1da62dc2e0e0ba6fd97e",
+    "url": "/covid-19-live/static/js/2.d7df4a55.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19-live/static/js/2.d7df4a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f18dec838fbc37a2ce6",
+    "url": "/covid-19-live/static/js/main.2f761e9c.chunk.js"
+  },
+  {
+    "revision": "74a526321d7deaaef0d7",
+    "url": "/covid-19-live/static/js/runtime-main.df7f6cf2.js"
+  }
+]);
